935ae27207a7e44d9e65a6dcaac6a95174e118f7
Ebi <kpemiogokimiebi@gmail.com>
2023-02-20 16:13:51 +0100

    Clean up auth module

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -5,10 +5,12 @@ export class AuthService {
 	async authenticate(username: string, password: string): Promise<boolean> {
 		// Perform authentication logic here (e.g., check against a database)
 		// Return true if the authentication is successful, false otherwise
+		return true;
 	}
 
 	async authorize(username: string, resource: string): Promise<boolean> {
 		// Perform authorization logic here (e.g., check against a database)
 		// Return true if the authorization is successful, false otherwise
+		return true;
 	}
 }

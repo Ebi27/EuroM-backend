58da41998f73d44d1c58601848898afcb169cc82
Ebi <kpemiogokimiebi@gmail.com>
2023-02-26 19:27:47 +0100

    Create prisma schema, initialize the schema and connect the prisma service to auth service. Create Login and Signup DTO's to validate incoming requests. Create functions to authenticate login, signup and signin requests.

---
+++ src/modules/auth/dtos/login.dto.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@nestjs/common'
+import {IsEmail, IsNotEmpty, IsString, Length} from 'class-validator'
+
+export class LoginDto {
+	@IsEmail()
+	public email: string
+
+	@IsNotEmpty()
+	@IsString()
+	@Length(8, 20, {
+		message: 'Password must be between 8 and 20 characters and contain a symbol and a number',
+	})
+	public password: string
+}
--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,16 +1,69 @@
 import { Injectable } from '@nestjs/common'
+import { BadRequestException } from '@nestjs/common/exceptions'
+import { PrismaService } from 'prisma/services/prisma.service'
+import { LoginDto } from '../dtos/login.dto'
+import { SignupDto } from '../dtos/signup.dto'
+import * as bcrypt from 'bcrypt'
+import { AuthToken } from '../interfaces/jwt-payload.interface'
+
 
 @Injectable()
 export class AuthService {
-	async authenticate(username: string, password: string): Promise<boolean> {
-		// Perform authentication logic here (e.g., check against a database)
-		// Return true if the authentication is successful, false otherwise
-		return true;
+	JwtService: any
+	constructor(private prisma: PrismaService, private jwtPayLoad: AuthToken) {}
+	async authenticateLogin(dto:LoginDto): Promise<any> {
+	const {email, password} = dto
+	const foundUser = await this.prisma.user.findUnique({where: {email}})
+	 if (!foundUser) {
+	 	throw new BadRequestException('User does not exist')
+		}
+		const passwordMatch = await this.comparePassword(
+			password, 
+			foundUser.hashedPassword
+			)
+		if (!passwordMatch) {
+			throw new BadRequestException('Incorrect email or password')
+		}
+		const token = await this.jwtPayLoad({ id: foundUser.id, email: foundUser.email })
+		return { token }
 	}
 
-	async authorize(username: string, resource: string): Promise<boolean> {
+	async authorizeLogin(username: string, resource: string): Promise<boolean> {
 		// Perform authorization logic here (e.g., check against a database)
 		// Return true if the authorization is successful, false otherwise
 		return true;
+	}
+	
+	async userSignup(dto:SignupDto): Promise<any> {	
+		const {email, username, password} = dto
+		const userExist = await this.prisma.user.findUnique({where: {email}})
+	if (userExist) {
+		throw new BadRequestException('User already exist')
+		}
+		const hashedPassword = await this.hashPassword(password)
+		const newUser = await this.prisma.user.create({
+			data: {
+				username,
+				email,	
+				hashedPassword,
+			},
+		})
+		return { message: 'User created successfully' }
+	}
+	
+	async userSignOut(): Promise<any> {	
+		// Perform authentication logic here (e.g., check against a database)
+		// Return true if the authentication is successful, false otherwise
+		return { message: 'User logged out successfully'}
+	}
+
+	async hashPassword(password: string) {
+		const saltOrRounds = 10
+		const hashedPassword = await bcrypt.hash(password, saltOrRounds)
+		return hashedPassword
+	}
+	async comparePassword(password: string, hashedPassword: string) {
+		const isPasswordMatch = await bcrypt.compare(password, hashedPassword)
+		return isPasswordMatch
 	}
 }

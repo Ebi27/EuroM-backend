0b5705fd1c2a439452cfd6d6270223e866541fc0
Ebi <kpemiogokimiebi@gmail.com>
2023-02-26 19:36:21 +0100

    Import utils, packages and methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { UserModule } from './modules/user/modules/user.module'
 import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from 'prisma/modules/prisma.module'
+import {JwtModule} from '@nestjs/jwt'
+import { AuthToken } from './modules/auth/Interfaces/Jwt-payload.Interface'
 
 @Module({
-	imports: [UserModule, AuthModule],
+	imports: [UserModule, AuthModule, PrismaModule, JwtModule, AuthToken],
 })
 export class AppModule {}

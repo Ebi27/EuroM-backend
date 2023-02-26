9519b49930646a3acfc7426d292448a9da957146
Ebi <kpemiogokimiebi@gmail.com>
2023-02-26 19:34:11 +0100

    Create controller function to handle the authentication routes for the login, signup, and signout requests. Create constant file to generate jwtSecret to be used in authenticating requests

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,29 +1,65 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body , Get, UseGuards} from '@nestjs/common'
 import { AuthService } from '../services/auth.service'
+import { LoginDto } from '../dtos/login.dto'
+import { SignupDto } from '../dtos/signup.dto'	
+import {JwtAuthGuard} from '../guards/jwt-auth.guard'
+
+
+// @Controller('protected')
+// export class ProtectedController {
+// 	@Get()
+// 	@UseGuards(JwtAuthGuard)
+// 	getProtectedData() {
+// 		return {message: 'This is protected data!'}
+// 	}
+// }
+
 
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post('login')
-	async login(@Body() body) {
-		const { username, password } = body
-		const success = await this.authService.authenticate(username, password)
-		if (success) {
+	async login(@Body() LoginDto: LoginDto) {
+		const {email, password} = LoginDto
+		const loginSuccessfull = await this.authService.authenticateLogin(LoginDto)
+		if (loginSuccessfull) {
 			// Generate a JWT or other token to represent the authenticated user
 		} else {
 			// Return an error indicating that the authentication failed
 		}
 	}
 
-	@Post('check')
-	async check(@Body() body) {
-		const { username, resource } = body
-		const authorized = await this.authService.authorize(username, resource)
-		if (authorized) {
+	@Post('lookup')
+	async lookup(@Body() body) {
+		const {username, resource} = body
+		const userIsAuthorized = await this.authService.authorizeLogin(username, resource)
+		if (userIsAuthorized) {
 			// Return a success message indicating that the user is authorized to access the resource
 		} else {
 			// Return an error indicating that the authorization failed
+		}
+	}
+
+	@Post('signup')
+	async signup(@Body() SignupDto: SignupDto) {
+		const {username, password} = SignupDto
+		const signupSuccessfull = await this.authService.userSignup(SignupDto)
+		if (signupSuccessfull) {
+			// Generate a JWT or other token to represent the authenticated user
+		} else {
+			// Return an error indicating that the authentication failed
+		}
+	}
+
+	@Get('signout')
+	async signout(@Body() body) {
+		const {username, password} = body
+		const signoutSuccessfull = await this.authService.userSignOut()
+		if (signoutSuccessfull) {
+			// Generate a JWT or other token to represent the authenticated user
+		} else {
+			// Return an error indicating that the authentication failed
 		}
 	}
 }

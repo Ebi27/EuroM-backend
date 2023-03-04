af1385ba5c8b9286d0dc7cc5135336202fb1be63
Ebi <kpemiogokimiebi@gmail.com>
2023-03-04 22:22:12 +0100

    Add additional path to the tsconfig to map prisma's alias to the actual path and to package file, so it can be resolved. Add cors to allow cross origin request but prevent pre-flight continue for security reasons

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common'
 import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create(AppModule, { cors: {
+		origin: true ,
+		preflightContinue: false,
+	}
+	})
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
 	app.use(cookieParser())
 	await app.listen(3000)

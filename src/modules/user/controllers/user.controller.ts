dfbbde59fe681a6177aa7f02b8cf8b8b95fb1680
Ebi <kpemiogokimiebi@gmail.com>
2023-03-04 22:09:09 +0100

    Create endpoints to handle and guard requests for retrieving user data

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param, UseGuards, Req } from '@nestjs/common'
+import { UserService } from '../services/user.service'
+import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard'
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor (private readonly userService: UserService) {}
+//create route to get a user by id 
+@UseGuards(JwtAuthGuard)
+@Get(':id')
+getUserById(@Param() params: { id: number }, @Req() req) {
+ return this.userService.getUserById(params.id, req)
+}
+
+
+//create route to get all users 
+@Get()
+getUsers(){
+    return this.userService.getUsers()
+}
+}

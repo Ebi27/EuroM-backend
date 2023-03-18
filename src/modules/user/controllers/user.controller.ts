01af73857a786b358e484fcee7ca09e7940c3a33
unknown <kpemiogokimiebi@gmail.com>
2023-03-18 23:17:52 +0100

    Create endpoints to receive and respond to requests relating to user details

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,24 +1,35 @@
-import { Controller, Get, Param, UseGuards, Req } from '@nestjs/common'
+import { Controller, Get, Param, UseGuards, Req, Patch, Delete } from '@nestjs/common'
 import { UserService } from '../services/user.service'
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard'
 
 @Controller('user')
 export class UserController {
-    constructor (private readonly userService: UserService) {}
+    constructor (private userService: UserService) {}
+
 @UseGuards(JwtAuthGuard)
-@Get(':id')
+@Get('/:id')
 getUserById(@Param() params: { id: number }, @Req() req) {
- return this.userService.findUser(params.id, req)
+ return this.userService.getUserById(params.id, req)
 }
 
 @UseGuards(JwtAuthGuard)
-@Get(':email')
+@Get('/:email')
 getUserByEmail(@Param() params: { email: string }, @Req() req) {
-    return this.userService.findByEmail(params.email, req)
+    return this.userService.getUserByEmail(params.email)
 }
 
 @Get()
-getUsers(){
+getAllUsers(){
     return this.userService.getUsers()
 }
+
+// @Patch('/:id')
+// updateUser(@Param() params: { id: number }, @Req() req) {
+//     return this.userService.updateUser(params.id, req)
+// }
+
+// @Delete('/:id')
+// deleteUser(@Param() params: { id: number }, @Req() req) {
+//     return this.userService.deleteUser(params.id, req)
+// }
 }
